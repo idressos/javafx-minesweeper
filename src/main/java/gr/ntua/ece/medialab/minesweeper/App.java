3b2dda6da256a9baf6586ace04e6eda4d286c4be
Ioannis Dressos <96877388+idressos@users.noreply.github.com>
2023-03-06 11:36:28 +0200

    Don't use MVC

--- src/main/java/gr/ntua/ece/medialab/minesweeper/App.java
+++ src/main/java/gr/ntua/ece/medialab/minesweeper/App.java
@@ -2,32 +2,24 @@ package gr.ntua.ece.medialab.minesweeper;
 
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 
-import javafx.fxml.FXMLLoader;
-
 import javafx.application.Application;
-
-import java.io.IOException;
 
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    public void start(Stage stage) {
+        Group group = new Group();
+        Scene scene = new Scene(group, 600, 300);
+        
+        stage.setTitle("MediaLab Minesweeper");
+        
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
