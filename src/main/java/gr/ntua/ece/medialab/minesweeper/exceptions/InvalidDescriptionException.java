2854877b58930e5c6b12595fdbd06b854c46cda9
Ioannis Dressos <96877388+idressos@users.noreply.github.com>
2023-03-06 13:01:06 +0200

    Add default serialVersionUID for custom exceptions

--- src/main/java/gr/ntua/ece/medialab/minesweeper/exceptions/InvalidDescriptionException.java
+++ src/main/java/gr/ntua/ece/medialab/minesweeper/exceptions/InvalidDescriptionException.java
@@ -2,6 +2,8 @@ package gr.ntua.ece.medialab.minesweeper.exceptions;
 
 public class InvalidDescriptionException extends Exception {
     
+    private static final long serialVersionUID = 1L;
+    
     public InvalidDescriptionException(String errorMessage, Throwable err) {
         super(errorMessage, err);
     }
--- src/main/java/gr/ntua/ece/medialab/minesweeper/exceptions/InvalidValueException.java
+++ src/main/java/gr/ntua/ece/medialab/minesweeper/exceptions/InvalidValueException.java
@@ -2,6 +2,8 @@ package gr.ntua.ece.medialab.minesweeper.exceptions;
 
 public class InvalidValueException extends Exception {
     
+    private static final long serialVersionUID = 1L;
+    
     public InvalidValueException(String errorMessage, Throwable err) {
         super(errorMessage, err);
     }
