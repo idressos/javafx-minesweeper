cbe3cddeb4ffc84dcdfcdcfe96d3b996ebca34da
Ioannis Dressos <96877388+idressos@users.noreply.github.com>
2023-03-07 20:11:19 +0200

    Create CountdownTimer class

---
+++ src/main/java/gr/ntua/ece/medialab/minesweeper/game/CountdownTimer.java
@@ -0,0 +1,52 @@
+package gr.ntua.ece.medialab.minesweeper.game;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import java.text.SimpleDateFormat;
+
+import javafx.scene.control.Label;
+
+public class CountdownTimer extends Label {
+	
+	private static Timer timer;
+	private static SimpleDateFormat displayFormat = new SimpleDateFormat("mm:ss");
+	
+	private static int secondsLeft = -1;
+	
+	public CountdownTimer() {
+		super();
+		setText("00:00");
+		setStyle("-fx-border-color: black");
+	}
+	
+	public void set(int sec) {
+		stop();
+		timer = new Timer();
+		
+		secondsLeft = sec;
+		
+		timer.schedule(updateTask, 0, 1000);
+	}
+	
+	public void stop() {
+		if(timer != null) {
+			timer.cancel();
+			timer.purge();
+		}
+		
+		timer = null;
+	}
+	
+	TimerTask updateTask = new TimerTask() {
+		@Override
+		public void run() {
+			if(secondsLeft > 0) {
+				secondsLeft--;
+				CountdownTimer.this.setText(displayFormat.format(new Date(secondsLeft * 1000)));
+			} else stop();
+	    }
+	};
+	
+}
